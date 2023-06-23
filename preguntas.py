f8ddb4acb67594e84eea79b5c111ceac75f65c5a
Cesar <caospinamu@unal.edu.co>
2023-06-22 20:56:41 -0500

    Laboratorio 8 terminado, primer intento

--- preguntas.py
+++ preguntas.py
@@ -104,7 +104,7 @@ def pregunta_03():
             ),
             # Paso 3: Construya un modelo de regresión lineal.
             (
-                "LinearRegression",
+                "linearRegression",
                 LinearRegression(),
             ),
         ],
